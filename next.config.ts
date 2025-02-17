243aa54ede7a9119dc83541bfc441625e9359bf0
Alan Courtois <alan@wesur.fr>
2025-02-17 23:05:36 +0100

    edit config for redirect not json path to json in /r/

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  rewrites: async () => {
+    return [
+      {
+        source: '/r/:path*',
+        destination: '/r/:path*.json',
+      },
+    ]
+},
 };
 
 export default nextConfig;
